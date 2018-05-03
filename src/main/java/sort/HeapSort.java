f180d8d6ac7a5389cd88dd559fd33be6c6eef6c9
baoling.li <baoling.li@aispeech.com>
2018-05-03 16:07:35 +0800

    堆排序优化，重用调整堆方法

--- src/main/java/sort/HeapSort.java
+++ src/main/java/sort/HeapSort.java
@@ -13,45 +13,53 @@ public class HeapSort implements Sort {
 	}
 	
 	//构建最大堆
+	//从最后一个节点开始，检查最后一个节点与其父节点是否符合，不符合则交换，交换后需要往下递归验证
+//	public void buildHeap(int[] nums){
+//		for(int i=nums.length-1;i>0;i--){
+//			int parent = (i-1)/2;
+//			//如果父节点较小，则交换
+//			if(nums[parent] < nums[i]){
+//				SwapUtils.swap(nums, i, parent);
+//				//交换后需要继续递归检查
+//				int temp = i;
+//				while(2*temp+1<nums.length){
+//					int max = 0;
+//					if(2*temp+2<nums.length){
+//						//符合则退出递归
+//						if(nums[temp]>=nums[2*temp+1] && nums[temp]>=nums[2*temp+2]){
+//							break;
+//						}
+//						max = nums[2*temp+1]>nums[2*temp+2]?2*temp+1:2*temp+2;
+//					}else{
+//						//符合则退出递归
+//						if(nums[temp]>=nums[2*temp+1]){
+//							break;
+//						}
+//						max = 2*temp+1;
+//					}
+//					SwapUtils.swap(nums, temp, max);
+//					temp = max;
+//				}
+//			}
+//		}
+//	}
+	
+	//由于叶子节点形成的树一定是堆，所以从最后一个非叶子节点开始，以该节点为根节点，使该树为堆，则需要使节点下沉校验。
 	public void buildHeap(int[] nums){
-		for(int i=nums.length-1;i>0;i--){
-			int parent = (i-1)/2;
-			//如果父节点较小，则交换
-			if(nums[parent] < nums[i]){
-				SwapUtils.swap(nums, i, parent);
-				//交换后需要继续递归检查
-				int temp = i;
-				while(2*temp+1<nums.length){
-					int max = 0;
-					if(2*temp+2<nums.length){
-						//符合则退出递归
-						if(nums[temp]>=nums[2*temp+1] && nums[temp]>=nums[2*temp+2]){
-							break;
-						}
-						max = nums[2*temp+1]>nums[2*temp+2]?2*temp+1:2*temp+2;
-					}else{
-						//符合则退出递归
-						if(nums[temp]>=nums[2*temp+1]){
-							break;
-						}
-						max = 2*temp+1;
-					}
-					SwapUtils.swap(nums, temp, max);
-					temp = max;
-				}
-			}
+		for(int i=nums.length/2-1;i>=0;i--){
+			adjustHeap(nums, i, nums.length);
 		}
 	}
 
 	public int pop(int[] nums,int n){
 		int value = nums[0];
 		SwapUtils.swap(nums, 0, n-1);
-		adjustHeap(nums,n-1);
+		adjustHeap(nums,0,n-1);
 		return value;
 	}
 	
-	public void adjustHeap(int[] nums, int n){
-		int i=0;
+	public void adjustHeap(int[] nums,int target, int n){
+		int i=target;
 		while(2*i+1<n){
 			int max=0;
 			if(2*i+2<n){
